d3b1ef01c2c6acfeda923e02adfc44369065bcdb
MyeongJun Kim <audwns990625@gmail.com>
2024-10-10 13:44:49 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/작은 구슬의 이동/small-marble-movement.cpp
+++ 241010/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,58 +1,52 @@
 #include <iostream>
 using namespace std;
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
+int dx[4] = {0, 0, -1, 1}; // L, R, U, D
+int dy[4] = {-1, 1, 0, 0}; // L, R, U, D
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n,t;
+    int n, t;
     cin >> n >> t;
-    int r=1,c=1;
+    int r, c;
     char d;
-    cin >> r>>c>>d;
-    int index;
-    if(d=='U')
-        index = 2;
+    cin >> r >> c >> d;
 
-    else if(d == 'D')    
-        index = 3;
-    
-    else if(d == 'R')
-        index =0;
-    
-    else
+    int index;
+    // 방향 설정: L = 0, R = 1, U = 2, D = 3
+    if (d == 'L') {
+        index = 0;
+    } else if (d == 'R') {
         index = 1;
-    
-    for(int i = 0 ;i<t;i++)
-    {
-        int dr, dc;
-        dr = r+dx[index];
-        dc = c+dy[index];
-        if(dr<1 || dr>n || dc < 1 || dc>n)
-        {
-            if(d=='U')
-            {
-                index = 3;
-            }
-            else if(d == 'D')
-            {
-                index = 2;
-            }
-            else if(d == 'R')
-            {
-                index =1;
-            }
-            else
-            {
-                index = 0;
-            }
-            continue;
-        }
-        r = dr;
-        c=  dc;
-        
+    } else if (d == 'U') {
+        index = 2;
+    } else {
+        index = 3;
     }
-    cout << r <<" " << c ;
+
+    for (int i = 0; i < t; i++) {
+        int dr = r + dx[index];
+        int dc = c + dy[index];
+
+        // 경계 체크: 배열 범위를 벗어나면 반대 방향으로 전환하고 계속 진행
+        if (dr < 1 || dr > n || dc < 1 || dc > n) {
+            // 반대 방향으로 전환
+            if (index == 0) {
+                index = 1; // L -> R
+            } else if (index == 1) {
+                index = 0; // R -> L
+            } else if (index == 2) {
+                index = 3; // U -> D
+            } else if (index == 3) {
+                index = 2; // D -> U
+            }
+            continue; // 전환 후 다시 시도
+        }
+
+        // 이동 좌표 업데이트
+        r = dr;
+        c = dc;
+    }
+
+    cout << r << " " << c;
     return 0;
 }
