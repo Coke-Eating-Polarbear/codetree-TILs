60e047a8e0577015f430e28e23b0673f2b89f432
MyeongJun Kim <audwns990625@gmail.com>
2024-10-10 13:42:57 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/작은 구슬의 이동/small-marble-movement.cpp
+++ 241010/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int dx[4] = {1,-1,0,0};
-int dy[4] = {0,0,1,-1};
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -13,27 +13,23 @@ int main() {
     cin >> r>>c>>d;
     int index;
     if(d=='U')
-    {
         index = 2;
-    }
-    else if(d == 'D')
-    {
+
+    else if(d == 'D')    
         index = 3;
-    }
+    
     else if(d == 'R')
-    {
         index =0;
-    }
+    
     else
-    {
         index = 1;
-    }
+    
     for(int i = 0 ;i<t;i++)
     {
         int dr, dc;
-        dr = r+dy[index];
-        dc = c+dx[index];
-        if(dr<1 || dr>n+1 || dc < 1 || dc>n+1)
+        dr = r+dx[index];
+        dc = c+dy[index];
+        if(dr<1 || dr>n || dc < 1 || dc>n)
         {
             if(d=='U')
             {
