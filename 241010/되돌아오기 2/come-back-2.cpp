d849411e9e6048d623c55e8aed0fef4d810b6270
MyeongJun Kim <audwns990625@gmail.com>
2024-10-10 14:30:33 +0900

    Title: 되돌아오기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int dx[4] = {-1, 0, 1, 0}; // W N E S 
+int dy[4] = {0, 1, 0, -1}; // W N E S 
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string input;
+    getline(cin, input);
+    int len = input.size();
+    int index = 1;
+    int x=0,y=0;
+    int nx,ny;
+    int count = 0;
+    for(int i = 0; i < len;i++)
+    {
+        if(input[i] == 'F')
+        {
+            nx = x + dx[index];
+            ny = y + dy[index];
+            count ++;
+        }
+        else if(input[i] == 'L')
+        {
+            index--;
+            count ++;
+            if(index <0)
+                index = 3;
+        }
+        else 
+        {
+            index++;
+            count ++;
+            if(index > 3)
+                index = 0;
+        }
+        
+        x = nx, y = ny;
+        if(x==0 && y==0)
+            break;
+    }
+    cout << count;
+    return 0;
+}
